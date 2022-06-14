bd830b66f3f679e42faa78caa1a9cbf14f093055
YU-AN CHEN <59487andy@gmail.com>
2022-06-14 23:09:29 +0800

    Update index.js

--- twdh_platform/src/modules/tdocusky/routes/index.js
+++ twdh_platform/src/modules/tdocusky/routes/index.js
@@ -1,6 +1,6 @@
 export default [
   {
-    path: '/tdocuSkyIndex',
+    path: '/tdocuskyIndex',
     name: 'TdocuSkyIndex',
     component: () => import(/* webpackChunkName: "tdocuSkyIndex" */ '../views/TdocuSkyIndex.vue')
   },
