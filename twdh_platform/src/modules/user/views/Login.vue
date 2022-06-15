6b76cbdba61dcc716c326319e687d5a2d63a95b6
Wei-Ji <sckhg1367@gmail.com>
2022-06-15 22:45:18 +0800

    add localstorage.

--- twdh_platform/src/modules/user/views/Login.vue
+++ twdh_platform/src/modules/user/views/Login.vue
@@ -97,6 +97,7 @@ export default {
               // storageObj 存入 Cookie
               $cookies.set("display_name", storageObj.display_name, "58min");
               $cookies.set("DocuSky_SID", storageObj.DocuSky_SID, "58min");
+              localStorage.setItem("display_name", storageObj.display_name);
 
               this.$store.commit('user/setUserName', `${res1.data.message.display_name} ~`);
             })    
