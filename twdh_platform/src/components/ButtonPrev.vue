92e535433e25631745ebc3375a55c0e37c657117
Wei-Ji <sckhg1367@gmail.com>
2022-06-15 17:28:38 +0800

    add ButtonNext, ButtonPrev button

---
+++ twdh_platform/src/components/ButtonPrev.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="button_system_prev_div">
+    <button class="button_system_prev" @click="prevPage">{{ label.label }}</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ButtonPrev',
+
+  props: {
+    "label": {
+      type: Object, // 試過String只能顯示數字的String，中英文字皆無法顯示，所以用Object
+      required: true
+    },
+  },
+  methods:{
+    prevPage(){
+      this.$router.push({ name: this.$router.currentRoute.value.meta.prev });
+    },
+  },
+}
+</script>
